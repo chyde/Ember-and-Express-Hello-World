ac345ee90d3efeb05c1749cd1fef5ce76df93018
Chris Hyde <chyde@vendnovation.com>
2018-04-02 16:51:45 -0700

    add mongodb get pizzas

--- api/app.js
+++ api/app.js
@@ -2,45 +2,25 @@ const express = require('express')
 var Validator = require('jsonapi-validator').Validator;
 var validator = new Validator();
 const app = express()
+const getPizzas = require('./controllers/controller');
 
-let pizzaData = 
-{
-  "data": [
-    { 
-      "id": "314",
-      "type": "pizza",
-      "attributes": { 
-          "name": "Pepperoni",
-          "description": "Best pizza ever" // for real
-      }
-    },
-    { 
-      "id": "315",
-      "type": "pizza",
-      "attributes": { 
-          "name": "Cheese",
-          "description": "Boring pizza"
-      }
-    },
-    { 
-      "id": "316",
-      "type": "pizza",
-      "attributes": { 
-          "name": "Veggie",
-          "description": "Salad"
-      }
-    }
-  ]
-};
 
 app.get("/pizzas", function(req, res)  { 
-    res.set('Content-Type', 'application/vnd.api+json');
+  getPizzas().then(function(pizzaData) {
+    // pack and send
+    //res.set('Content-Type', 'application/vnd.api+json');
     // NOTE: CORS for testing
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", 
+      "Origin, X-Requested-With, Content-Type, Accept");
 
-    if(! validator.isValid(pizzaData)) console.log("Ruh roh!  Bad JSON-API data");
-    res.send(pizzaData);
+    // HACK: removes nested object references by stringifying and re-parsing)
+    if(! validator.isValid(JSON.parse(JSON.stringify(pizzaData)))) {
+      console.warn("Ruh roh!  Bad JSON-API data");
+      res.send({ data: [] }); 
+    }
+    res.send(pizzaData); 
+  });
 });
-
+ 
 app.listen(3000, () => console.log('Pizza at port 3000!'));
