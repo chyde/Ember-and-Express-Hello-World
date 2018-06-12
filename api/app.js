8e54054082f94963551dda06308a20e5fa10aa7c
Chris Hyde <chyde@vendnovation.com>
2018-06-11 17:36:58 -0700

    Add Delete CRUD action

--- api/app.js
+++ api/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 var Validator = require('jsonapi-validator').Validator;
 var validator = new Validator();
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const app = express()
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({
+  type: 'application/vnd.api+json'
+}));
 
 const SchemaManager = require("./SchemaManager");
 const pizzaController = require('./controllers/PizzaController');
@@ -12,38 +14,42 @@ const settings = require('./settings');
 
 
 var addCors = function(res) {
-    // NOTE: CORS for testing
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", 
-      "Origin, X-Requested-With, Content-Type, Accept");
+  // NOTE: CORS for testing
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
 }
 
 SchemaManager.execute();
 
-app.get("/pizzas", function(req, res)  { 
+app.get("/pizzas", function(req, res) {
   pizzaController.getPizzas().then(function(pizzaData) {
     addCors(res);
 
     // HACK: removes nested object references by stringifying and re-parsing)
     var dataString = JSON.parse(JSON.stringify(pizzaData));
-    var jsonApiPizzas = { data: dataString }
-    
-    // Validate and handle json api
-    if(! validator.isValid(jsonApiPizzas)) {
-      console.warn("ERROR: Bad JSON-API data:", pizzaData);
-      res.send({ data: [] }); 
+    var jsonApiPizzas = {
+      data: dataString
     }
 
-    res.send(jsonApiPizzas); 
+    // Validate and handle json api
+    if (!validator.isValid(jsonApiPizzas)) {
+      console.warn("ERROR: Bad JSON-API data:", pizzaData);
+      res.send({
+        data: []
+      });
+    }
+
+    res.send(jsonApiPizzas);
   });
 });
 
-app.options("/pizzas", function(req, res)  { 
+app.options("/pizzas/*", function(req, res) {
   addCors(res);
   res.send();
 });
 
-app.post("/pizzas", function(req, res)  {
+app.post("/pizzas", function(req, res) {
   addCors(res);
 
   pizzaController.addPizza(req.body.data.attributes, function(result) {
@@ -51,6 +57,15 @@ app.post("/pizzas", function(req, res)  {
   })
 });
 
+app.delete("/pizzas/:pizzaId", function(req, res) {
+  console.log("DELETING A PIZZA");
+  console.log(req.params);
+  pizzaController.deletePizza(req.params.pizzaId, function(result) {
+    res.status(204)
+      .send({});
+  });
+});
 
- 
+
+
 app.listen(3000, () => console.log('Pizza at port 3000!'));
--- api/controllers/PizzaController.js
+++ api/controllers/PizzaController.js
@@ -1,25 +1,28 @@
-var MongoClient = require('mongodb');
+var MongoClient = require('mongodb'),
+  ObjectID = require('mongodb').ObjectID;
+
 var assert = require('assert');
 const settings = require('../settings');
 
 var PizzaController = {
-  
+
   getPizzas: function() {
     return new Promise(function(resolve, reject) {
-        var db = MongoClient.connect(settings.dbUrl, function(err, client) {
+        MongoClient.connect(settings.dbUrl, function(err, client) {
           const db = client.db(settings.databaseName);
 
           resolve(db.collection('pizzas').find({}, {
             name: 1,
             description: 1
           }).toArray());
+          db.close();
         });
       })
       .then(function(pizzas) {
         var pizzaData = [];
 
         for (var pizzaIndex = pizzas.length - 1; pizzaIndex >= 0; pizzaIndex--) {
-          pizzaData.push(mongoToJsonApi(pizzas[pizzaIndex], "pizza"))
+          pizzaData.push(PizzaController.mongoToJsonApi(pizzas[pizzaIndex], "pizza"))
         }
 
         return pizzaData;
@@ -42,7 +45,7 @@ var PizzaController = {
     MongoClient.connect(settings.dbUrl, function(err, client) {
       const db = client.db(settings.databaseName);
 
-      var newPizza = db.collection('pizzas')
+      db.collection('pizzas')
         .insert({
           name: pizza.name,
           description: pizza.description
@@ -51,6 +54,24 @@ var PizzaController = {
           callback({
             data: pizzaJsonApi
           });
+          db.close();
+        });
+    });
+  },
+
+  deletePizza: function(pizzaId, callback) {
+    MongoClient.connect(settings.dbUrl, function(err, client) {
+      const db = client.db(settings.databaseName);
+
+      db.collection('pizzas')
+        .deleteOne({
+          _id: ObjectID(pizzaId)
+        }, function(err, result) {
+          console.log("err", err);
+          assert.equal(err, null);
+          assert.equal(1, result.result.n);
+          console.log("Removed the document" + result);
+          callback(result);
           db.close();
         });
     });
